fb8ec6d3cde6e017c7aa24e79a3cf6a3b1db003d
GaryChenMesser <gary9192631770@gmail.com>
2018-04-14 00:14:26 +0800

    runnable

--- week7/fbfried.py
+++ week7/fbfried.py
@@ -53,37 +53,44 @@ class FacebookCrawler:
         # navigate to "about" page
         self.wait.until(EC.visibility_of_element_located((By.XPATH, '//a[@data-tab-key="about"]')))
         self.driver.find_element_by_xpath('//a[@data-tab-key="about"]').click()
-        
+        print('in1')
         # check whether this user is in NTUEE.
-        self.wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@class="_c24 _50f4"]/a')))
-        about = self.driver.find_elements_by_xpath('//div[@class="_c24 _50f4"]/a')
+        try:
+            self.wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@class="_c24 _50f4"]')))
+            about = self.driver.find_elements_by_xpath('//div[@class="_c24 _50f4"]')
         
-        flag = 0
-        
-        for item in about:
+            print('in2')
             flag = 0
-            for phrase in NTU:
-                if phrase in item.text:
-                    flag += 1
-                    break
             
-            for phrase in EE:
-                if phrase in item.text:
-                    flag += 1
-                    break
+            for item in about:
+                flag = 0
+                print(item.text)
+                for phrase in self.NTU:
+                    if phrase in item.text:
+                        flag += 1
+                        break
+                
+                for phrase in self.EE:
+                    if phrase in item.text:
+                        flag += 1
+                        break
+                
+                if flag == 2:
+                    return True
             
-            if flag == 2:
-                return True
+            return False
         
-        return False
+        except:
+            return False
     
     def get_ref(self):
-        try:
-            return self.driver.find_element_by_xpath('//a[@class="_2nlw"]').get_attribute('href')
-        except:
-            print('Can only call get_ref in profile!')
+        #try:
+            self.wait.until(EC.visibility_of_element_located((By.XPATH, '//a[@class="_2nlw _2nlv"]')))
+            return self.driver.find_element_by_xpath('//a[@class="_2nlw _2nlv"]').get_attribute('href')
+        #except:
+        #    print('Can only call get_ref in profile!')
     
-    def get_user(self, href):
+    def change_user(self, href):
         self.driver.get(href)
 
     def _get_friends_list(self):
@@ -117,9 +124,14 @@ class FacebookCrawler:
 class FriendGraph:
     G = Graph(directed=False)
     v_href = G.new_vertex_property('string') # record href
+    # create property maps
     v_visited = G.new_vertex_property('bool') # record visited or not
     v_deleted = G.new_vertex_property('bool') # record deleted or not
     v_source = G.new_vertex_property('int') # record the previous friendship
+    # make property maps internal
+    G.vertex_properties["visited"] = v_visited
+    G.vertex_properties["deleted"] = v_deleted
+    G.vertex_properties["source"] = v_source
     
     def __init__(self, ref):
         index = self.G.add_vertex()
@@ -127,71 +139,104 @@ class FriendGraph:
         self.v_visited[index] = False
         self.v_deleted[index] = False
         self.cursor = 0
-        self.href_dict = {v_href[index]: index}
+        self.href_dict = {self.v_href[index]: index}
     
-    def check_visited(self, index):
-        if index >= len(self.v_visited):
-            print("index out of range\n")
-            return False
-        return v_visited[index]
+    def existed(self, ref):
+        if ref in self.href_dict.keys():
+            return True
+        return False
+    
+    def get_visited(self, ref):
+        if ref in self.href_dict.keys():
+            return self.v_visited[self.href_dict[ref]]
         
-    def check_deleted(self, index):
-        if index >= len(self.v_deleted):
-            print("index out of range\n")
-            return False
-        return v_deleted[index]
+        print(ref)
+        print("Unknown visited key\n")
+        return -1
+        
+    def get_deleted(self, ref):
+        if ref in self.href_dict.keys():
+            return self.v_deleted[self.href_dict[ref]]
+        
+        print(ref)
+        print("Unknown deleted key\n")
+        return -1
     
     def get_index(self, ref):
         if ref in self.href_dict.keys():
-            return self.href_dict[ref]
+            return self.href_dict[self.href_dict[ref]]
         
-        print("Unknown key\n")
+        print(ref)
+        print("Unknown index key\n")
         return -1
     
-    def add_vertex(self, ref):
+    def add_vertex(self, ref, source_ref):
         index = self.G.add_vertex()
         self.v_href[index] = ref
         self.v_visited[index] = False
         self.v_deleted[index] = False
-        self.href_dict = {v_href[index]: index}
+        self.v_source[index] = self.href_dict[source_ref]
+        self.href_dict[self.v_href[index]] = index
     
-    def visit(sef, ref):
+    def add_edge(self, ref):
+        self.G.add_edge(self.href_dict[ref], self.v_source[self.href_dict[ref]])
+    
+    def visit(self, ref):
         self.v_visited[self.href_dict[ref]] = True
     
     def delete(self, ref):
         self.v_deleted[self.href_dict[ref]] = True
     
+    def save(self, filename):
+        self.G.save(filename)
 
+    def inf(self):
+        print("number of vertex: ", self.G.num_vertices())
+        print("number of edge: ", self.G.num_edges())
+        
 
-if __name__ == '__main__':
+def main():
     crawler = FacebookCrawler(login = sys.argv[1], password = sys.argv[2] + ' ' + sys.argv[3])
-    last_deque = deque()
-    graph = FriendGraph(crawler.get_ref())
-    
-    
-    try:
-        for step in range(sys.argv[4]):
-            
-        
-        
-            if len(old_deque) == 0:
-                this_deque = deque(crawler.get_friends())
-            
+    print('check0')
+    ref = crawler.get_ref()
+    old_deque = deque([ref])
+    graph = FriendGraph(ref)
+    print('check1')
+    #try:
+    for a in range(1):
+        for step in range(int(sys.argv[4])):
+            this_deque = deque()
+            print('deque: ', len(old_deque))
             while len(old_deque) > 0:
-                user = old_pop()
-                crawler.get_user(user)
-                graph.visit(user)
+                user = old_deque.pop()
                 
+                if graph.get_visited(user) or graph.get_deleted(user):
+                    continue
+                
+                print('while1')
+                if step != 0:
+                    crawler.change_user(user)
+                graph.visit(user)
+                print('while2')
                 if not crawler.check_user():
                     graph.delete(user)
                     continue
-                
-                for friend in crawler.get_friends():
-                    this_deque.append(friend)
+                if step != 0:
+                    graph.add_edge(user)
+                print('while3')
+                for new in crawler.get_friends():
+                    if not graph.existed(new):
+                        graph.add_vertex(new, user)
+                        this_deque.append(new)
             
-            #old_deque = this_deque
-            
+            old_deque = this_deque
         
-        
-    except:
-        
+        graph.save(sys.argv[1] + '_' + sys.argv[4] + '.gt')
+        print('Complete all steps and save successfully!')
+        print(graph.inf())
+    #except:
+    #    graph.save(sys.argv[1] + '_' + sys.argv[4] + 'but' + str(step) +'_temp.gt')
+    #    print('Exception raised but save temp file successfully.')
+
+if __name__ == '__main__':
+    main()
